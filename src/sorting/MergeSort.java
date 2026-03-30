7c9c938403ccebe852dfe3c9dd8dae23c0c677bb
Janarthan135289 <janakumar2002@gmail.com>
2026-03-30 21:53:32 +0530

    Merge Sort add comments

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -3,43 +3,46 @@ package sorting;
 import java.util.Arrays;
 
 public class MergeSort {
-    static void merge(int[] arr,int[] l,int[] r,int left,int right){
-        int i=0,j=0,k=0;
-        while(i<left && j<right){
-            if(l[i]<r[j]){
-                arr[k++]=l[i++];
-            }
-            else{
-                arr[k++]=r[j++];
+    // Merging the individual parts
+    static void merge(int[] arr, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] < r[j]) {
+                arr[k++] = l[i++];
+            } else {
+                arr[k++] = r[j++];
             }
         }
-        while(i<left){
-            arr[k++]=l[i++];
+        while (i < left) {
+            arr[k++] = l[i++];
         }
-        while(j<right){
-            arr[k++]=r[j++];
+        while (j < right) {
+            arr[k++] = r[j++];
         }
     }
-    static void mergeSort(int[] arr,int n){
-        if(n<2)
-            return;
-        int mid=n/2;
-        int[] l=new int[mid];
-        int[] r=new int[n-mid];
-        for(int i=0;i<mid;i++){
-            l[i]=arr[i];
-        }
-        for(int j=mid;j<n;j++){
-            r[j-mid]=arr[j];
-        }
-        mergeSort(l,mid);
-        mergeSort(r,n-mid);
 
-        merge(arr,l,r,mid,n-mid);
+    // Split the array into subarrays
+    static void mergeSort(int[] arr, int n) {
+        if (n < 2)
+            return;
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+        for (int i = 0; i < mid; i++) {
+            l[i] = arr[i];
+        }
+        for (int j = mid; j < n; j++) {
+            r[j - mid] = arr[j];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(arr, l, r, mid, n - mid);
     }
+
     public static void main(String[] args) {
-        int[] arr={100,40,90,20,10};
-        mergeSort(arr,arr.length);
+        int[] arr = {100, 40, 90, 20, 10};
+        mergeSort(arr, arr.length);
         System.out.println(Arrays.toString(Arrays.stream(arr).toArray()));
     }
 }
